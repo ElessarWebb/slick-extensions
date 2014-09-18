09d5dc71ca5b8ade65e2b920e6f97360394d1eb9
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-09-18 19:03:41 +0200

    Moved implicits into TableWithId

--- src/main/scala/aj/slick/tables/WithId.scala
+++ src/main/scala/aj/slick/tables/WithId.scala
@@ -6,17 +6,13 @@ trait TableWithId { self: Profile =>
 
   import profile.simple._
 
-  object implicits extends HasIdImplicits
-
   trait HasId[Id] { self: Table[_] =>
     def id: Column[Id]
   }
 
-  trait HasIdImplicits {
-    implicit class HasIdQueryExt[Id: BaseColumnType, U]
-    (query: TableQuery[_ <: Table[U] with HasId[Id]]) {
-      def filterById(id: Id)(implicit s: Session) = query.filter(_.id === id)
-      def insertReturnId(m: U)(implicit s: Session) = query.returning(query.map(_.id)) += m
-    }
+  implicit class HasIdQueryExt[Id: BaseColumnType, U]
+  (query: TableQuery[_ <: Table[U] with HasId[Id]]) {
+    def filterById(id: Id)(implicit s: Session) = query.filter(_.id === id)
+    def insertReturnId(m: U)(implicit s: Session) = query.returning(query.map(_.id)) += m
   }
 }
