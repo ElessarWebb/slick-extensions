2a788828ca9adc7567a9438f48f48292eb14d059
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-09-19 17:23:12 +0200

    Minor fix JodaSupport

--- src/main/scala/aj/slick/utils/GenericJodaSupport.scala
+++ src/main/scala/aj/slick/utils/GenericJodaSupport.scala
@@ -2,6 +2,7 @@ package aj.slick.utils
 
 import aj.slick.Profile
 import com.github.tototoshi.slick.GenericJodaSupport
+import scala.slick.driver.JdbcDriver
 
 trait JodaSupport { self: Profile =>
   object jodamappers extends GenericJodaSupport(profile.asInstanceOf[JdbcDriver])
